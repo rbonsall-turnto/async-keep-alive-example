06b6470b3c03cd6a64ddf25b1a3b874559bed59b
rbonsall <rbonsall@turnto.com>
2021-04-01 21:19:43 -0700

    upgrade Vue to latest, fix tab active

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="tab">
-    <button class="tablink" :class="{'active': component === 'hello-world'}" @click="showHello">Hello World</button>
-    <button class="tablink" :class="{'active': component === 'hello-world-async'}" @click="showHelloAsync">Hello World Async</button>
-    <button class="tablink" :class="{'active': component === 'goodby-world-async'}" @click="showGoodbyeAsync">Goodbye World Async</button>
-  </div>
-  <div class="tabcontent">
-    <keep-alive :include="['hello-world', 'hello-world-async']">
-      <component :is="component"></component>
-    </keep-alive>
+  <div class="main">
+    <div class="tab">
+      <button class="tablink" :class="{'active': component === 'hello-world'}" @click="showHello">Hello World</button>
+      <button class="tablink" :class="{'active': component === 'hello-world-async'}" @click="showHelloAsync">Hello World Async</button>
+      <button class="tablink" :class="{'active': component === 'goodbye-world-async'}" @click="showGoodbyeAsync">Goodbye World Async</button>
+    </div>
+    <div class="tabcontent">
+      <keep-alive :include="['hello-world', 'hello-world-async']">
+        <component :is="component"></component>
+      </keep-alive>
+    </div>
   </div>
 </template>
 
@@ -54,9 +56,12 @@ export default {
     margin-top: 60px;
   }
 
-  /* client tab styles */
-  .tab {
+  .main {
+    margin: 0 auto;
     max-width: 1000px;
+  }
+  
+  .tab {
     overflow: hidden;
     border: 1px solid #ccc;
     background-color: #f1f1f1;
@@ -81,7 +86,6 @@ export default {
   }
 
   .tabcontent {
-    max-width: 1000px;
     padding: 6px 12px;
     border: 1px solid #ccc;
     border-top: none;
